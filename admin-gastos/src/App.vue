e97b39c6d5e6d9fbb25ec4e205ccb08b19992a76
Anderson <andres.salazar.p@uni.pe>
2023-08-04 11:54:24 -0500

    Video 109

---
+++ admin-gastos/src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import Presupuesto from './components/Presupuesto.vue'
+import { ref } from 'vue'
+
+const presupuesto=ref(0);
+const definirPresupuesto=(cantidad)=>{
+    presupuesto.value=cantidad
+}
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Planificador de Gastos</h1>
+      <div class="contenedor-header contenedor sombre">
+      <Presupuesto 
+      v-if="presupuesto===0"
+      @definir-presupuesto="definirPresupuesto">
+      </Presupuesto>
+      <p v-else>Presupuesto Valido</p>
+      </div>
+      
+    </header>
+  </div>
+</template>
+
+<style>
+  :root{
+    --azul: #3b82f6;
+    --blanco: #fff;
+    --gris-claro: #f5f5f5;
+    --gris: #94a3b8;
+    --gris-oscuro: #64748b;
+    --negro: #000;
+  }
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *:after{
+    box-sizing: inherit;
+  }
+  body{
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--gris-claro);
+  }
+  h1{
+    font-size:4rem;
+  }
+  h2{
+    font-size:3rem;
+  }
+  header{
+    background-color: var(--azul);
+  }
+  header h1{
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--blanco);
+    text-align: center;
+  }
+  .contenedor{
+    width: 90%;
+    max-width: 80rem;
+    border-radius: 2rem;
+    margin: 0 auto;
+    background-color: rgb(255, 255, 255);
+  }
+  .contenedor-header{
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+  }
+  .sombre{
+    box-shadow:0px 10px 15px -3px rgba(0, 0, 0, 0.1)
+  }
+</style>
---
+++ admin-gastos/src/components/Presupuesto.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { ref } from 'vue'
+import Alerta from './Alerta.vue'
+const presupuesto=ref(0);
+const error=ref();
+const emit=defineEmits(['definir-presupuesto'])
+const definirPresupuesto=()=>{
+    if(presupuesto.value<=0){
+    
+        error.value="Presupuesto no valido"
+        setTimeout(()=>{
+            error.value=""
+        },3000)
+    
+    }
+    emit('definir-presupuesto',presupuesto.value)
+
+}
+
+</script> 
+<template>
+    <form class="presupuesto"
+    @submit.prevent="definirPresupuesto">
+        <Alerta v-if="error">
+            {{ error }}
+        </Alerta>
+        <div class="campo">
+            <label for="nuevo-presupuesto">
+                Definir Presupuesto
+            </label>
+            <input 
+            id="nuevo-presupuesto"
+            class="nuevo-presupuesto"
+            placeholder="Añade tu presupuesto"
+            type="number"
+            v-model="presupuesto"
+            >
+        </div>
+        <input type="submit" value="Definir Presupuesto">
+
+    </form>
+
+</template>
+<style scoped>
+    .presupuesto{
+        width: 100%;
+    }
+    .campo{
+        display: grid;
+        gap:2rem;
+        background-color: ;
+    }
+    .campo label{
+        font-size: 2.5rem;
+        text-align: center;
+        color: var(--azul);
+    }
+    .presupuesto input[type="number"]{
+        background-color: var(--gris-claro); 
+        border-radius: 1rem;
+        padding: 1rem;
+        border: none;
+        font-size: 2.2rem;
+        width: 100%;
+        text-align: center;
+    }
+    .presupuesto input[type="submit"]{
+        background-color: var(--azul);
+        border: none;
+        padding: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 2rem;
+        color: var(--blanco);
+        font-weight: 900;
+        width: 100%;
+        transition: all ease-in-out 300ms;
+    }
+    .presupuesto input[type="submit"]:hover{
+        background-color: rgba(35, 0, 236, 0.918);
+        cursor: pointer;
+    }
+</style>
